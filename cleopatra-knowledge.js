9e58dfeee999441e58c28dd0b4cd5a107de2bcd8
ParasolNordic <arkisto.kaksi@gmail.com>
2025-11-26 00:47:33 +0200

    Create cleopatra-knowledge.js

---
+++ cleopatra-knowledge.js
@@ -0,0 +1,7 @@
+export const CleopatraKnowledge = {
+  name: "Kleopatra VII Filopator",
+  age: 39,
+  date: "Heinäkuu 30 eaa.",
+  location: "Aleksandria, Egypti"
+  // ... (lisää loput JSON-dataa myöhemmin)
+};
